90f5fad51f7800734d46571a9db1eee6114b8fac
Mateus Macedo Dos Anjos <macedodosanjosmateus@gmail.com>
2024-12-01 13:51:09 -0300

    feat(flux): ✨ add pipeline stages and classes
    This commit adds the necessary pipeline stages and classes to the flux library. It includes the following changes:
    - Added the PipelineStage interface for defining pipeline stages
    - Added the Pipeline class for executing stages
    - Added the ProcessorChain class for chaining stages together
    - Added the TransformationStage class for data transformation
    - Added the ValidationStage class for data validation

---
+++ libs/flux/src/index.ts
@@ -0,0 +1,5 @@
+export * from './lib/interface/pipeline-stage';
+export * from './lib/pipeline';
+export * from './lib/processor-chain';
+export * from './lib/stages/transformation-stage';
+export * from './lib/stages/validation-stage';
