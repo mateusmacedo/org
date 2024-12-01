ba5268bc1d83c22ffdc4f70c2d5c1d7e1f4ab414
Mateus Macedo Dos Anjos <macedodosanjosmateus@gmail.com>
2024-12-01 16:21:39 -0300

    feat(flux): :sparkles: add ProcessorChain class
    The ProcessorChain class is added to the `libs/flux` library. This class allows for chaining multiple processors together to process input data. Each processor is executed asynchronously, and the output of one processor is passed as input to the next. This addition enables modular and flexible data processing pipelines.

---
+++ libs/flux/src/lib/processor-chain.ts
@@ -0,0 +1,36 @@
+import { Stage } from './interface/pipeline-stage';
+import { Observable } from 'rxjs';
+
+export class ProcessorChain<I, O> {
+  private processors: Stage<any, any>[] = [];
+
+  addProcessor<T>(processor: Stage<I, T>): ProcessorChain<I, T> {
+    this.processors.push(processor);
+    return this as unknown as ProcessorChain<I, T>;
+  }
+
+  process(input: I): Observable<O> {
+    return new Observable<O>(subscriber => {
+      let current = input;
+
+      const processNext = async (index: number) => {
+        if (index >= this.processors.length) {
+          subscriber.next(current as unknown as O);
+          subscriber.complete();
+          return;
+        }
+
+        const processor = this.processors[index];
+        try {
+          const result = await processor.execute(current);
+          current = result;
+          processNext(index + 1);
+        } catch (error) {
+          subscriber.error(error);
+        }
+      };
+
+      processNext(0);
+    });
+  }
+}
